4555428470bd0ae1951aff886bfb29443e317353
Farouk Farag <ffarag@proemcs.com>
2021-06-27 01:07:59 +0200

    Add TokenizerWrap class in separate file.

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.applications import InceptionV3
 import pickle
 
-
 def load_image(img, size=None):
 
     if not size is None:
@@ -39,46 +38,10 @@ transfer_values_size = K.int_shape(transfer_layer.output)[1]
 mark_start = 'start '
 mark_end = ' end'
 
-class TokenizerWrap(Tokenizer):
-    """Wrap the Tokenizer-class from Keras with more functionality."""
-    
-    def __init__(self, texts, num_words=None):
+from TokenizerWrap import TokenizerWrap
 
-        Tokenizer.__init__(self, num_words=num_words)
-
-        self.fit_on_texts(texts)
-        # Create inverse lookup from integer-tokens to words.
-        self.index_to_word = dict(zip(self.word_index.values(),
-                                      self.word_index.keys()))
-
-    def token_to_word(self, token):
-        """Lookup a single word from an integer-token."""
-
-        word = " " if token == 0 else self.index_to_word[token]
-        return word 
-
-    def tokens_to_string(self, tokens):
-        """Convert a list of integer-tokens to a string."""
-
-        # Create a list of the individual words.
-        words = [self.index_to_word[token]
-                 for token in tokens
-                 if token != 0]
-        
-
-        text = " ".join(words)
-        return text
-    
-    def captions_to_tokens(self, captions_listlist):
-       
-        tokens = [self.texts_to_sequences(captions_list)
-                  for captions_list in captions_listlist]
-        return tokens
-
-
-if __name__ == '__main__':
-    with open('tokenizer.pkl', 'rb') as input:
-        tokenizer = pickle.load(input)
+with open('tokenizer.pkl', 'rb') as input:
+    tokenizer = pickle.load(input)
 
 num_words = 15000
 token_start = tokenizer.word_index[mark_start.strip()]
