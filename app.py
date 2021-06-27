8ab7cb9eec21c5e52ff75735ec17e815cce2931b
Farouk Farag <ffarag@proemcs.com>
2021-06-27 21:12:14 +0200

    Fix tokenizer bug.

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ tf.disable_v2_behavior()
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import Model, load_model
 from tensorflow.python.keras.layers import Input, Dense, GRU, Embedding
+from tensorflow.python.keras.callbacks import ModelCheckpoint, TensorBoard
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.applications import InceptionV3
+import pickle
 import dill
+from tensorflow.python.keras.layers import Dropout,GRU
 
 def load_image(img, size=None):
-
     if not size is None:
         img = img.resize(size=size, resample=Image.LANCZOS)
 
@@ -38,9 +40,43 @@ transfer_values_size = K.int_shape(transfer_layer.output)[1]
 mark_start = 'start '
 mark_end = ' end'
 
-from TokenizerWrap import TokenizerWrap
+class TokenizerWrap(Tokenizer):
+    """Wrap the Tokenizer-class from Keras with more functionality."""
+    
+    def __init__(self, texts, num_words=None):
 
-with open('tokenizer.pkl', 'rb') as input:
+        Tokenizer.__init__(self, num_words=num_words)
+
+        self.fit_on_texts(texts)
+        # Create inverse lookup from integer-tokens to words.
+        self.index_to_word = dict(zip(self.word_index.values(),
+                                      self.word_index.keys()))
+
+    def token_to_word(self, token):
+        """Lookup a single word from an integer-token."""
+
+        word = " " if token == 0 else self.index_to_word[token]
+        return word 
+
+    def tokens_to_string(self, tokens):
+        """Convert a list of integer-tokens to a string."""
+
+        # Create a list of the individual words.
+        words = [self.index_to_word[token]
+                 for token in tokens
+                 if token != 0]
+        
+
+        text = " ".join(words)
+        return text
+    
+    def captions_to_tokens(self, captions_listlist):
+       
+        tokens = [self.texts_to_sequences(captions_list)
+                  for captions_list in captions_listlist]
+        return tokens
+
+with open('TokenizerDill.pkl', 'rb') as input:
     tokenizer = dill.load(input)
 
 num_words = 15000
@@ -53,8 +89,6 @@ def sparse_cross_entropy(y_true, y_pred):
                                                           logits=y_pred)
     loss_mean = tf.reduce_mean(loss)
     return loss_mean
-
-from tensorflow.python.keras.layers import Dropout,CuDNNGRU
 
 state_size = 512
 embedding_size = 128
@@ -72,13 +106,13 @@ decoder_embedding = Embedding(input_dim=num_words,
                               output_dim=embedding_size,
                               name='decoder_embedding')
 
-decoder_gru1 = CuDNNGRU(state_size, name='decoder_gru1',
+decoder_gru1 = GRU(state_size, name='decoder_gru1',
                    return_sequences=True)
-decoder_gru2 = CuDNNGRU(state_size, name='decoder_gru2',
+decoder_gru2 = GRU(state_size, name='decoder_gru2',
                    return_sequences=True)
-decoder_gru3 = CuDNNGRU(state_size, name='decoder_gru3',
+decoder_gru3 = GRU(state_size, name='decoder_gru3',
                    return_sequences=True)
-decoder_gru4 = CuDNNGRU(state_size, name='decoder_gru4',
+decoder_gru4 = GRU(state_size, name='decoder_gru4',
                    return_sequences=True)
 
 decoder_dense = Dense(num_words,
@@ -122,9 +156,9 @@ except Exception as error:
     print("Error trying to load checkpoint.")
     print(error)
 
-def generate_caption(image_path, max_tokens=30):
+def generate_caption(image_path_img, max_tokens=30):
 
-    image = load_image(image_path, size=img_size)
+    image = load_image(image_path_img, size=img_size)
 
     image_batch = np.expand_dims(image, axis=0)
 
@@ -155,10 +189,7 @@ def generate_caption(image_path, max_tokens=30):
     
     predicted_caption=output_text.split()
     del (predicted_caption[-1])
-    
-    print("Predicted caption:")
-    print(output_text)
-    print()
+
     return output_text
 
 
